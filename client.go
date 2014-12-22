0abbd93cfb64f6558510b19a3e781ea75f4a826e
Lyonel Vincent <lyonel@ezix.org>
2014-12-22 11:23:11 +0100

    more code reorg: split remote commands handling

--- client.go
+++ client.go
@@ -21,29 +21,6 @@ import (
 var directories map[string]bool
 var backupset map[string]struct{}
 
-func remotecommand(arg ...string) *exec.Cmd {
-	os.Setenv("SSH_CLIENT", "")
-	os.Setenv("SSH_CONNECTION", "")
-
-	if cfg.Server != "" {
-		cmd := []string{"-q", "-C", "-oBatchMode=yes", "-oStrictHostKeyChecking=no", "-oUserKnownHostsFile=/dev/null"}
-		if cfg.User != "" {
-			cmd = append(cmd, "-l", cfg.User)
-		}
-		cmd = append(cmd, cfg.Server)
-		cmd = append(cmd, programName)
-		cmd = append(cmd, arg...)
-		return exec.Command("ssh", cmd...)
-	} else {
-
-		if exe, err := os.Readlink("/proc/self/exe"); err == nil {
-			return exec.Command(exe, arg...)
-		} else {
-			return exec.Command(programName, arg...)
-		}
-	}
-}
-
 func expire() {
 	flag.Int64Var(&date, "date", date, "Backup set")
 	flag.Int64Var(&date, "d", date, "-date")
