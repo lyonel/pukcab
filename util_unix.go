f9bef3b383308e3d1d49c9d1d33452b7a36dd9d1
Lyonel Vincent <lyonel@ezix.org>
2014-12-26 12:38:30 +0100

    implement username/groupname lookup

--- util_unix.go
+++ util_unix.go
@@ -6,9 +6,40 @@ import (
 
 /*
 #include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <grp.h>
+
+static char *getusername(int uid)
+{
+  struct passwd *pw = getpwuid(uid);
+
+  if(!pw)
+	return "";
+  else
+	return pw->pw_name;
+}
+
+static char *getgroupname(int gid)
+{
+  struct group *gr = getgrgid(gid);
+
+  if(!gr)
+	return "";
+  else
+	return gr->gr_name;
+}
 */
 import "C"
 
 func IsATTY(f *os.File) bool {
 	return C.isatty(C.int(f.Fd())) != 0
+}
+
+func Username(uid int) (username string) {
+	return C.GoString(C.getusername(C.int(uid)))
+}
+
+func Groupname(gid int) (groupname string) {
+	return C.GoString(C.getgroupname(C.int(gid)))
 }
