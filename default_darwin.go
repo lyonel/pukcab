e95c45b566b226677847b6c0dfe9198194e3109f
Lyonel Vincent <lyonel@ezix.org>
2015-02-01 00:25:19 +0100

    add (semi-empty) OS X specific functions

---
+++ default_darwin.go
@@ -0,0 +1,4 @@
+package main
+
+var defaultInclude = []string{"hfs"}
+var defaultExclude = []string{"devfs", "autofs", "afpfs", "./.nobackup"}
---
+++ util_darwin.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"os"
+	"syscall"
+)
+
+import "C"
+
+func Attributes(file string) (result []string) {
+	result = make([]string, 0)
+
+	return
+}
+
+func Attribute(file string, name string) (result []byte) {
+	result = make([]byte, 0)
+
+	return
+}
+
+func DevMajorMinor(file string) (major int64, minor int64) {
+	var st syscall.Stat_t
+
+	syscall.Stat(file, &st)
+
+	major = int64(st.Rdev>>24) & ^0xff
+	minor = int64(st.Rdev&0xffffff)
+	return
+}
+
+func IsNodump(fi os.FileInfo, file string) bool {
+	if fi.Mode()&(os.ModeTemporary|os.ModeSocket) != 0 {
+		return true
+	}
+	if !(fi.Mode().IsDir() || fi.Mode().IsRegular()) {
+		return false
+	}
+	return false
+}
