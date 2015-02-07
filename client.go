40062c33921b008db13da97d36163c29039997de
Lyonel Vincent <lyonel@ezix.org>
2015-02-07 16:31:56 +0100

    code reorg

--- client.go
+++ client.go
@@ -10,7 +10,6 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
-	"math"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -465,30 +464,6 @@ func dumpfiles() {
 		fmt.Println("done.")
 	}
 
-}
-
-func logn(n, b float64) float64 {
-	return math.Log(n) / math.Log(b)
-}
-
-func human(s uint64, base float64, sizes []string) string {
-	if s < 10 {
-		return fmt.Sprintf("%dB", s)
-	}
-	e := math.Floor(logn(float64(s), base))
-	suffix := sizes[int(e)]
-	val := math.Floor(float64(s)/math.Pow(base, e)*10+0.5) / 10
-	f := "%.0f%s"
-	if val < 10 {
-		f = "%.1f%s"
-	}
-	return fmt.Sprintf(f, val, suffix)
-}
-
-// Bytes produces a human readable representation of an byte size.
-func Bytes(s uint64) string {
-	sizes := []string{"B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"}
-	return human(s, 1024, sizes)
 }
 
 func info() {
--- util.go
+++ util.go
@@ -109,3 +109,27 @@ func ConvertGlob(filter string) (regex string) {
 
 	return
 }
+
+func logn(n, b float64) float64 {
+	return math.Log(n) / math.Log(b)
+}
+
+func human(s uint64, base float64, sizes []string) string {
+	if s < 10 {
+		return fmt.Sprintf("%dB", s)
+	}
+	e := math.Floor(logn(float64(s), base))
+	suffix := sizes[int(e)]
+	val := math.Floor(float64(s)/math.Pow(base, e)*10+0.5) / 10
+	f := "%.0f%s"
+	if val < 10 {
+		f = "%.1f%s"
+	}
+	return fmt.Sprintf(f, val, suffix)
+}
+
+// Bytes produces a human readable representation of an byte size.
+func Bytes(s uint64) string {
+	sizes := []string{"B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"}
+	return human(s, 1024, sizes)
+}
