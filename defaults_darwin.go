6dc31b0f21d8fce500220cd41829ceaff7f5cf6b
Lyonel Vincent <lyonel@ezix.org>
2015-04-06 21:19:01 +0200

    update default exclusions of OS X

--- defaults_darwin.go
+++ defaults_darwin.go
@@ -1,4 +1,24 @@
 package main
 
 var defaultInclude = []string{"hfs"}
-var defaultExclude = []string{"devfs", "autofs", "afpfs", "./.nobackup"}
+var defaultExclude = []string{
+	"afpfs",
+	"autofs",
+	"devfs",
+	"smbfs",
+	"/.fseventsd",
+	"/.Spotlight-V100",
+	"/.Trashes",
+	"/.vol",
+	"/private/tmp",
+	"/private/var/db/BootCaches",
+	"/private/var/db/systemstats",
+	"/private/var/folders",
+	"/private/var/tmp",
+	"/private/var/vm",
+	"/Library/Caches",
+	"/System/Library/Caches",
+	"/Users/*/Library/Caches",
+	"./fseventsd-uuid",
+	"./.nobackup",
+}
