7c352b183df027a88062ea5835325ac0867d1e8c
Lyonel Vincent <lyonel@ezix.org>
2015-04-14 17:31:29 +0200

    remove dependency on math.Log() (should fix SIGILL on Pentium III)

--- util.go
+++ util.go
@@ -115,22 +115,22 @@ func ConvertGlob(name string, filters ...string) (SQL string) {
 	return strings.Join(clauses, " OR ")
 }
 
-func logn(n, b float64) float64 {
-	return math.Log(n) / math.Log(b)
-}
-
 func human(s uint64, base float64, sizes []string) string {
 	if s < 10 {
-		return fmt.Sprintf("%dB", s)
+		return fmt.Sprintf("%d%s", s, sizes[0])
 	}
-	e := math.Floor(logn(float64(s), base))
-	suffix := sizes[int(e)]
-	val := math.Floor(float64(s)/math.Pow(base, e)*10+0.5) / 10
+	val := float64(s)
+	suffix := 0
+
+	for val >= base {
+		val = val / base
+		suffix++
+	}
 	f := "%.0f%s"
 	if val < 10 {
 		f = "%.1f%s"
 	}
-	return fmt.Sprintf(f, val, suffix)
+	return fmt.Sprintf(f, val, sizes[suffix])
 }
 
 // Bytes produces a human readable representation of an byte size.
