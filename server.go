850c85999f0e738b910d39e6b121b54c5db8b251
Lyonel Vincent <lyonel@ezix.org>
2015-06-13 18:16:59 +0200

    improve logging: report `submitfiles` duration

--- server.go
+++ server.go
@@ -378,6 +378,7 @@ func submitfiles() {
 	}
 
 	files := 0
+	started := time.Now()
 	catalog.QueryRow("SELECT COUNT(*) FROM files WHERE backupid=?", date).Scan(&files)
 
 	if files == 0 {
@@ -506,10 +507,10 @@ func submitfiles() {
 			catalog.Exec("UPDATE backups SET finished=? WHERE date=?", time.Now().Unix(), date)
 			catalog.Exec("UPDATE backups SET files=(SELECT COUNT(*) FROM files WHERE backupid=date) WHERE date=?", date)
 			catalog.Exec("UPDATE backups SET size=(SELECT SUM(size) FROM files WHERE backupid=date) WHERE date=?", date)
-			log.Printf("Finished backup: date=%d name=%q schedule=%q files=%d received=%d\n", date, name, schedule, files, received)
+			log.Printf("Finished backup: date=%d name=%q schedule=%q files=%d received=%d duration=%.0f\n", date, name, schedule, files, received, time.Since(started).Seconds())
 			fmt.Printf("Backup %d complete (%d files)\n", date, files)
 		} else {
-			log.Printf("Received files for backup set: date=%d name=%q schedule=%q files=%d missing=%d received=%d\n", date, name, schedule, files, missing, received)
+			log.Printf("Received files for backup set: date=%d name=%q schedule=%q files=%d missing=%d received=%d duration=%.0f\n", date, name, schedule, files, missing, received, time.Since(started).Seconds())
 			fmt.Printf("Received %d files for backup %d (%d files to go)\n", files-missing, date, missing)
 		}
 	} else {
