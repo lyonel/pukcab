e4eb86cf1d92e109b4e08d596c19e32525de8820
Lyonel Vincent <lyonel@ezix.org>
2018-12-04 10:44:19 +0100

    add ZFS to default Linux filesystems

--- src/pukcab/defaults_linux.go
+++ src/pukcab/defaults_linux.go
@@ -1,4 +1,4 @@
 package main
 
-var defaultInclude = []string{"ext2", "ext3", "ext4", "btrfs", "xfs", "jfs", "vfat"}
+var defaultInclude = []string{"ext2", "ext3", "ext4", "btrfs", "xfs", "jfs", "vfat", "zfs"}
 var defaultExclude = []string{"/proc", "/sys", "/selinux", "tmpfs", "./.nobackup", "/var/cache/yum", "/var/cache/dnf"}
